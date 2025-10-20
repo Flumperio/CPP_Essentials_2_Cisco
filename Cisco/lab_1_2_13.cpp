ec5595fbe56e47ab70bd76f7996a895cb69738f0
Juan Carlos <jcs0969@icloud.com>
2025-10-20 19:18:19 +0200

    lab_2_13_XX.cpp

---
+++ Cisco/lab_1_2_13.cpp
@@ -0,0 +1,115 @@
+// *****************************************
+// Created by jcsantos on 10/15/25.
+// Curso Cisco: c++ Essentials 1
+// 1.2.12   LAB   Flight booking system: part 1
+//
+// Scenario
+//
+// Let's continue working on our booking system.
+// You may remember in our last task that we had no limit on the number of reserved seats.
+// Airlines often allow the overbooking of flights, expecting that some passengers will not make it.
+// Modify the constructor so that it will not allow more than 105% reservation of the total capacity.
+// Also for a negative number of reservations, set the number to 0.
+// We might also want to be able to add new reservations or cancel them. Provide a way for the user to do this via a simple interface.
+//     The command "add [n]" will try to add n reservations to the flight.
+//     The command "cancel [n]" will try to cancel n reservations from the flight.
+//     If an operation fails for any reason, the program will issue the message "Cannot perform this operation"
+//     The command "quit" will stop execution of the program.
+// Use the code from the previous lab as a starting point.
+// *****************************************
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+class Reporte {
+private:
+    int     id_vuelo;
+    int     plazas_disponibles;
+    int     plazas_reservadas;
+    int     plazas_overbooking;
+
+public:
+    // El constructor lo podemos inicializar de dos maneras, en este caso recibe tres valores, y los incializamos,
+    // en la declaración o en el cuerpo. Es más eficiente lo primero, si no hay que operar sobre los valores.
+    Reporte(int int_vuelo, int int_disponibles, int int_reservadas): 
+        id_vuelo(int_vuelo), plazas_disponibles(int_disponibles), 
+        plazas_reservadas(int_reservadas), plazas_overbooking(plazas_disponibles*1.05)
+    {
+        if (plazas_reservadas > plazas_overbooking)
+            plazas_reservadas = (plazas_disponibles * 105) / 100;
+    }
+
+    void    imprimir_estado(){
+        cout << "Numero Vuelo: " << id_vuelo << endl
+        << " " << plazas_reservadas << "/" << plazas_disponibles <<
+        "(" << ((plazas_reservadas * 100) / plazas_disponibles ) << "%) asientos reservados" << endl;
+    }
+    void    add_plazas(int add_plazas){
+        this->plazas_reservadas += add_plazas;
+    }
+    void    cancel_plazas(int cancel_plazas){
+        this->plazas_reservadas  -= cancel_plazas;
+        }
+   
+};
+
+void str_lower (string &entrada){
+    for (char &c : entrada)
+        c = tolower(c);
+}
+
+bool verificar_entrada(string entrada, string& accion, int& cantidad) {
+    size_t  pos_espacio = 0;
+    string  parte_numerica = "";
+    if (entrada == "quit"){
+        accion = "";
+        cantidad = 0;
+        return false;
+}
+    str_lower(entrada);
+    pos_espacio = entrada.find(' ');
+    if (pos_espacio == string::npos)
+        return true;
+    accion = entrada.substr(0, pos_espacio);
+    parte_numerica = entrada.substr(pos_espacio + 1);
+    if (parte_numerica.empty() || parte_numerica.find_first_of("01234567890") == string::npos)
+        return true;
+    cantidad = stoi(parte_numerica);
+    return false;
+}
+
+void modificar_datos(Reporte& vuelo){
+    string  entrada_usuario;
+    string  accion;
+    int     cantidad;
+    bool    comando_erroneo = false;
+    do {
+        cout << "Que quieres hacer ?: ";
+        getline(cin, entrada_usuario);
+        comando_erroneo = verificar_entrada(entrada_usuario, accion, cantidad);
+        if (comando_erroneo)
+            cout << "Error de comando. Usar add XX, cancel XX o quit." << endl;
+        else if (accion == "add")
+            vuelo.add_plazas(cantidad);
+        else if (accion == "cancel")
+            vuelo.cancel_plazas(cantidad);
+    } while (entrada_usuario != "quit");
+}
+
+int main () {
+    int     id = 1;
+    int     disponibles = 0;
+    int     reservadas = 0;
+    cout << "Introduce el número de plazas disponibles: ";
+    cin >> disponibles;
+    cout << "Introduce el número de reservas: ";
+    cin >> reservadas;
+    cin.ignore();
+    Reporte Reporte_Vuelos(id, disponibles, reservadas);
+    Reporte_Vuelos.imprimir_estado();
+    modificar_datos(Reporte_Vuelos);
+    Reporte_Vuelos.imprimir_estado();
+    return 0;
+}
